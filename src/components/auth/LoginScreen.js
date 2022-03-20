91ce0ed21fc6cc7356bed3c14c7cf2264a0a4ace
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-20 20:22:17 +0100

    created initial login form

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 export const LoginScreen = () => {
   return (
-    <div>
-        <h1>LoginScreen</h1>
-    </div>
+    <>
+        <h1>Login</h1>
+
+        <form>
+
+          <input 
+            type="text"
+            placeholder="email"
+            name="email"
+          />
+
+          <input 
+            type="password"
+            placeholder="password"
+            name="password"
+          />
+
+          <button
+            type="submit"
+          >
+            Login
+          </button>
+
+          <hr />
+          google
+        </form>
+    </>
   )
 }
