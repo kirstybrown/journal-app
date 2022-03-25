62075594be8340f0c6277a31d88c6628ee8f1fe0
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-25 19:57:21 +0100

    added content and image to NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -7,6 +7,29 @@ export const NoteScreen = () => {
 
         <NotesAppBar />
 
+        <div className='notes__content'>
+
+            <input 
+                type="text"
+                placeholder="Some awesome title"
+                className="notes__title-input"
+            />
+
+            <textarea
+                placeholder='What happened today'
+                className='notes__textarea'
+            ></textarea>
+
+            <div className='notes__image'>
+                <img
+                    src="https://lh3.googleusercontent.com/pNKOucw9LP44ZwuUQGC1kpuLSm35tx9X19iaiytfMW3iSz-veYMVVIiegj592SqV-xmqLhnu6-fHyMD7b4hGjyepDQ0=w640-h400-e365-rj-sc0x00ffffff"
+                    alt="mountains"
+                />
+
+            </div>
+
+        </div>
+
     </div>
   )
 }
