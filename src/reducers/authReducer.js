880089bd8de7dfbd79e4a26d06f53b4459034369
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-25 20:37:27 +0100

    installed redux, redux-react, and created types for login and logout

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+/* 
+    {
+        uid: adfg246,
+        name: 'Fernando
+    }
+
+*/
+
+export const authReducer = ( state = {}, action ) => {
+
+    switch ( action.type ) {
+        case types.login:
+            return {
+                uid: action.payload.uid,
+                name: action.payload.displayName
+            }
+
+        case types.logout:
+            return { }
+    
+        default:
+            break;
+    }
+}
