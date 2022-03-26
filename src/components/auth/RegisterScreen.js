ba6c2b7dc341a1427fd5e9f77d8153fb87295bf7
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-26 21:02:56 +0100

    configured error messages through Redux

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import validator from 'validator';
-import { Link } from 'react-router-dom'
-import { useForm } from '../../hooks/useForm'
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { removeError, setError } from '../../actions/ui';
 
 export const RegisterScreen = () => {
+
+  const dispatch = useDispatch();
 
   const [ formValues, handleInputChange ] = useForm({
     name: 'Kirsty',
@@ -25,16 +29,17 @@ export const RegisterScreen = () => {
   const isFormValid = () => {
 
     if ( name.trim().length === 0 ) {
-      console.log('Name is required');
+      dispatch( setError('Name is required') );
       return false;
     } else if ( !validator.isEmail( email ) ) {
-      console.log("Email is not valid");
+      dispatch( setError("Email is not valid") );
       return false;
     } else if ( password !== password2 || password.length < 5 ) {
-      console.log("Password should be at least 6 characters and match each other");
+      dispatch( setError("Password should be at least 6 characters and match each other") );
       return false;
     }
 
+    dispatch( removeError() );
     return true;
   }
 
