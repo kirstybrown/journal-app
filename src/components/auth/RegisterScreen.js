6636bbcbfad76fa155809de86323be0a1a9cca17
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-26 20:30:41 +0100

    added useForm to form to register users

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,12 +1,33 @@
+import { getDefaultNormalizer } from '@testing-library/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
 
 export const RegisterScreen = () => {
+
+  const [ formValues, handleInputChange ] = useForm({
+    name: 'Kirsty',
+    email: 'kirsty@gmail.com',
+    password: '12345',
+    password2: '12345'
+  });  
+  
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+  }
+
+  const isFormValid = () => {
+    
+  }
+
   return (
     <>
         <h3 className='auth__title'>Register</h3>
 
-        <form>
+        <form onSubmit={ handleRegister }>
 
           <input 
             type="text"
@@ -14,6 +35,8 @@ export const RegisterScreen = () => {
             name="name"
             className='auth__input'
             autoComplete="off"
+            value={ name }
+            onChange={ handleInputChange }
           />
 
           <input 
@@ -22,6 +45,8 @@ export const RegisterScreen = () => {
             name="email"
             className='auth__input'
             autoComplete="off"
+            value={ email }
+            onChange={ handleInputChange }
           />
 
           <input 
@@ -30,6 +55,8 @@ export const RegisterScreen = () => {
             name="password"
             className='auth__input'
             autoComplete="off"
+            value={ password }
+            onChange={ handleInputChange }
           />
           <input 
             type="password"
@@ -37,12 +64,13 @@ export const RegisterScreen = () => {
             name="password2"
             className='auth__input'
             autoComplete="off"
+            value={ password2 }
+            onChange={ handleInputChange }
           />
 
           <button
             type="submit"
             className='btn btn-primary btn-block mb-5'
-            disabled={true}
           >
             Register
           </button>
