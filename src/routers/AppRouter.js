8be5ccc63d3a9f5acd809a110c08beb7bb00ed45
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-04-03 20:20:27 +0200

    show loading in login screen and state as loading or loggedin

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -13,15 +13,30 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch();
 
+    const [checking, setChecking] = useState(true)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+
     useEffect(() => {
       
         firebase.auth().onAuthStateChanged( (user) => {
 
             if ( user?.uid ) {
                 dispatch( login( user.uid, user.displayName ) );
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
             }
+
+            setChecking(false);
         } );      
-    }, [dispatch])
+    }, [dispatch, setChecking, setIsLoggedIn])
+
+    if ( checking ) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
     
 
   return (
