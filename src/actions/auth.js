14143f249a591dcb49c83a4ce10792542ad74464
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-04-03 19:55:16 +0200

    defined new loading actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,18 +1,21 @@
 import { firebase, googleAuthProvider } from "../firebase/firebase-config";
 import { types } from "../types/types";
+import { uiFinishLoading, uiStartLoading } from "./ui";
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
 
+        dispatch( uiStartLoading() );
+
         firebase.auth().signInWithEmailAndPassword( email, password )
             .then( async({ user }) => {
 
-                dispatch(
-                    login( user.uid, user.displayName )
-                )
+                dispatch( login( user.uid, user.displayName ) );
+                dispatch( uiFinishLoading() );
             } )
             .catch(e => {
                 console.log(e);
+                dispatch( uiFinishLoading() );
             })
     }
 }
